935a1a2298fc6a70fd40442d3d8e1743464fd494
Mars1327 <148791029+Mars1327@users.noreply.github.com>
2023-10-23 18:35:56 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WriteConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello");
+
+            Console.Write("World");
+
+            Console.Write("!!!");
+
+            Console.ReadKey();
+
+        }
+    }
+}
