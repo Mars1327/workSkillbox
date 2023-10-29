59c2c7919abba3e13a95c7ee817f52c48eab0e97
Mars1327 <148791029+Mars1327@users.noreply.github.com>
2023-10-29 15:43:37 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,23 +1,41 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Collections;
 
-namespace WriteConsoleApp1
+
+
+namespace homeWorkC_2_7
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.Write("Hello");
+            string fullName = "Sinitsyn Anatoliy Aleksandrovich";
+            byte age = 40;
+            string email = "sin1094606@yandex.ru";
+            byte scoreProgram = 87;
+            byte scoreMathem = 92;
+            byte scorePhysics = 82;
 
-            Console.Write("World");
+            string pattern = "ФИО: {0} \nВозраст: {1} \nEmail: {2} \nБаллы по программированию: {3} \nБаллы по математике: {4} \nБаллы по физике: {5} ";
+            Console.WriteLine(pattern,
+                              fullName,
+                              age,
+                              email,
+                              scoreProgram,
+                              scoreMathem,
+                              scorePhysics);
 
-            Console.Write("!!!");
+            Console.ReadLine();
 
-            Console.ReadKey();
+            int sumAllScore = scoreProgram + scoreMathem + scorePhysics;
+            double averageScore = sumAllScore / 3;
+
+            Console.WriteLine($"Сумма баллов: {sumAllScore} \nСредний балл: {averageScore}");
+            
+            Console.ReadLine();
 
         }
+
     }
 }
